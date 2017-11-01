749f6b40f0c1b251198984ca64cf25b1c8460b23
Aqshal Fajar Putra <aqshalfajarputra0226@gmail.com>
2017-11-01 22:23:16 +0700

    Add Marker

--- app/src/main/java/id/sch/smktelkom_mlg/learn/googlemapsapi_xiir3_05/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/googlemapsapi_xiir3_05/MainActivity.java
@@ -13,10 +13,9 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-
     static final CameraPosition INDONESIA = CameraPosition.builder()
             .target(new LatLng(-6.175392, 106.827178))
             .zoom(17)
@@ -43,11 +42,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             .build();
     GoogleMap m_map;
     boolean mapReady = false;
+    MarkerOptions Indonesia, France, UnitedState, Australia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Indonesia = new MarkerOptions()
+                .position(new LatLng(-6.175392, 106.827178))
+                .title("Monumen Nasional");
+
+        France = new MarkerOptions()
+                .position(new LatLng(48.858270, 2.294509))
+                .title("Eiffel Tower");
+
+        UnitedState = new MarkerOptions()
+                .position(new LatLng(38.897678, -77.036477))
+                .title("The White House");
+
+        Australia = new MarkerOptions()
+                .position(new LatLng(-33.856820, 151.215279))
+                .title("Sydney Opera House");
 
         Button bAustralia = (Button) findViewById(R.id.bAustalia);
         bAustralia.setOnClickListener(new View.OnClickListener() {
@@ -89,6 +105,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapReady = true;
         m_map = map;
         m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        m_map.addMarker(Indonesia);
+        m_map.addMarker(France);
+        m_map.addMarker(UnitedState);
+        m_map.addMarker(Australia);
         flyTo(INDONESIA);
     }
 
